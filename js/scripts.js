a59eb298d768dbb46255d9f1daa39025e7c0c3f2
JM Stump <jmstump@gmail.com>
2021-01-12 14:05:40 -0800

    add an additonal clickable peek-a-boo that properly works.
    Co-Authored-By: Alan Call <alanmatthewcall@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
-  });
+  })
+  $(".click").click(function() {
+    $("#cat-showing").toggle();
+    $("#cat-hidden").toggle();
+  }); 
 });
